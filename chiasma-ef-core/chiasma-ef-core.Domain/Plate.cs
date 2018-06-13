d9617301417ab6931ac3b0f29a3b37680767c593
GitEdvard <edvard.englund@medsci.uu.se>
2018-06-13 11:19:08 +0200

    Add model for plate single table
    In the chiasma-ef-core.Data project. Command: scaffold-dbcontext "data
    source=130.238.178.226;integrated security=true;initial
    catalog=gtdb2_devel_ee;" Microsoft.EntityFrameworkCore.SQLServer -tables
    "plate" -startupproject "chiasma-ef-core.data". Manually move domain class to the domain project.

---
+++ chiasma-ef-core/chiasma-ef-core.Domain/Plate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace chiasma_ef_core.Data
+{
+    public partial class Plate
+    {
+        public int PlateId { get; set; }
+        public string PlateUsage { get; set; }
+        public int PlateTypeId { get; set; }
+        public string Identifier { get; set; }
+        public string Comment { get; set; }
+        public string Status { get; set; }
+        public int? BeadChipInfoId { get; set; }
+        public int? SampleSeriesId { get; set; }
+        public int? PlateNumber { get; set; }
+        public string Method { get; set; }
+    }
+}
