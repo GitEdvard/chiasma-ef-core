496b5e3f6c3a19322624ea54a24e087cf4eded75
GitEdvard <edvard.englund@medsci.uu.se>
2018-06-13 10:56:00 +0200

    Add full model of chiasma db in separate project
    Command: scaffold-dbcontext "data source=130.xxx;integrated security=true;initial catalog=gtdbxxx;" Microsoft.EntityFrameworkCore.SQLServer -startupproject "chiasma-ef-core.fulldata"

--- chiasma-ef-core/chiasma-ef-core.FullData/Properties/AssemblyInfo.cs
+++ chiasma-ef-core/chiasma-ef-core.FullData/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("068e153e-6b9e-4785-a0ea-d0fb8438c865")]
+[assembly: Guid("9ce7adfc-712a-4b89-9eac-7f7d7128af7b")]
 
 // Version information for an assembly consists of the following four values:
 //
