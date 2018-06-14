a468950ab461ddd861f92dc959862ed6421aa763
GitEdvard <edvard.englund@medsci.uu.se>
2018-06-14 12:18:20 +0200

    Add project for unit test
    Installing Nunit framework

---
+++ chiasma-ef-core/chiasma-ef-core.UnitTest/InMemoryContextTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace chiasma_ef_core.UnitTest
+{
+    [TestFixture]
+    class InMemoryContextTests
+    {
+        [Test]
+        public void InitializeDbContext()
+        {
+
+        }
+    }
+}
