f905616de4ad0f206992a144a2c57723a1845818
GitEdvard <edvard.englund@medsci.uu.se>
2018-06-14 07:59:32 +0200

    First migration
    Giving command add-migration init -startupproject chiasma-ef-core.Data.
    Then, go to the migration file and delete content in up() and down()
    methods. Note that there is a snapshot that is necessary.

---
+++ chiasma-ef-core/chiasma-ef-core.Data/Migrations/20180614054539_init.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace chiasma_ef_core.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        { }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
