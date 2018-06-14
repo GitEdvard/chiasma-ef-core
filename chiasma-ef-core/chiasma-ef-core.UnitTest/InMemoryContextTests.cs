27b910b04677d552d27d13d554db844a6b39bd03
GitEdvard <edvard.englund@medsci.uu.se>
2018-06-14 14:14:47 +0200

    Add unit test with use in memory database

--- chiasma-ef-core/chiasma-ef-core.UnitTest/InMemoryContextTests.cs
+++ chiasma-ef-core/chiasma-ef-core.UnitTest/InMemoryContextTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using chiasma_ef_core.Data;
 
 namespace chiasma_ef_core.UnitTest
 {
@@ -11,9 +13,32 @@ namespace chiasma_ef_core.UnitTest
     class InMemoryContextTests
     {
         [Test]
-        public void InitializeDbContext()
+        public void AddOnePlate()
         {
-
+            var dbOptionsBuilder = new DbContextOptionsBuilder<gtdb2_devel_eeContext>();
+            dbOptionsBuilder.UseInMemoryDatabase("MyTestDb");
+            using (var context = new gtdb2_devel_eeContext(dbOptionsBuilder.Options))
+            {
+                var plate = new Plate();
+                plate.PlateId = 0;
+                plate.Identifier = "mytestplate";
+                context.Plates.Add(plate);
+                context.SaveChanges();
+            }
+        }
+        [Test]
+        public void AddAnotherPlate()
+        {
+            var dbOptionsBuilder = new DbContextOptionsBuilder<gtdb2_devel_eeContext>();
+            dbOptionsBuilder.UseInMemoryDatabase("MyTestDb");
+            using (var context = new gtdb2_devel_eeContext(dbOptionsBuilder.Options))
+            {
+                var plate = new Plate();
+                plate.PlateId = 1;
+                plate.Identifier = "mytestplat2e";
+                context.Plates.Add(plate);
+                context.SaveChanges();
+            }
         }
     }
 }
