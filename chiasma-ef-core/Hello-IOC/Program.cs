e1a2a4c9af9c5e7c65f4c898015fa1b6766cd059
GitEdvard <edvard.englund@medsci.uu.se>
2018-06-25 12:58:54 +0200

    Instantiate code with IOC container
    Create code with two simple related classes and instantiate with a IOC
    container, in different ways. Map different implementations of
    ICreditCard to the Shopper class, with help of the IOC container.

--- chiasma-ef-core/Hello-IOC/Program.cs
+++ chiasma-ef-core/Hello-IOC/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StructureMap;
 
 namespace Hello_IOC
 {
     class Program
     {
+        /// <summary>
+        /// See also code for the domain driven design course. There they use
+        /// structuremap in order to implement Domain Events. 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            CreateWithAction();
+            CreateWithDefaultConvention();
+            CreateWithRegistry();
+        }
+
+        private static void CreateWithAction()
+        {
+            var container = new Container(_ => { _.For<ICreditCard>().Use<Visa>(); });
+            var shopper = container.GetInstance<Shopper>();
+            shopper.Charge();
+        }
+
+        private static void CreateWithDefaultConvention()
+        {
+            // Make CreditCard public, that made the trick.
+            var container = new Container(_ =>
+            {
+                _.Scan(x =>
+                {
+                    x.TheCallingAssembly();
+                    x.WithDefaultConventions();
+                });
+            });
+            var shopper = container.GetInstance<Shopper>();
+            shopper.Charge();
+        }
+
+        private static void CreateWithRegistry()
+        {
+            var container = new Container(new MyRegistry());
+            var shopper = container.GetInstance<Shopper>();
+            shopper.Charge();
+        }
+
+        class MyRegistry : Registry
+        {
+            public MyRegistry()
+            {
+                For<ICreditCard>().Use<MasterCard>();
+            }
         }
     }
 }
---
+++ chiasma-ef-core/Hello-IOC/Shopper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Hello_IOC
+{
+    class Shopper
+    {
+        private ICreditCard _card;
+
+        public Shopper(ICreditCard card)
+        {
+            _card = card;
+        }
+
+        public void Charge()
+        {
+            Console.WriteLine(_card.Charge());
+        }
+    }
+
+    public class CreditCard : ICreditCard
+    {
+        public string Charge()
+        {
+            return "Charge with default creditcard";
+        }
+    }
+
+    class MasterCard : ICreditCard
+    {
+        public string Charge()
+        {
+            return "Charge with MasterCard";
+        }
+    }
+    class Visa : ICreditCard
+    {
+        public string Charge()
+        {
+            return "Charge with visa";
+        }
+    }
+
+    internal interface ICreditCard
+    {
+        string Charge();
+    }
+}
